934c87858a648f27654cb00e2b53a7cf34236f70
Ozcan YILDIRIM <ozcan.y@onthegosystems.com>
2025-01-24 20:46:32 +0300

    Finalize the Reversed-IP app task

--- terraform/live/acm.tf
+++ terraform/live/acm.tf
@@ -1,7 +1,7 @@
 module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "~> 4.0"
-  domain_name  = "hyprime.io"
+  domain_name  = "*.hyprime.io"
   zone_id      = "Z04985752S18IFBLX5I7P"
   validation_method = "DNS"
   wait_for_validation = true
